58fc18f9dbd870fb351d286a357619d612b81c25
Ryan Fanning <fanningrm@gmail.com>
2014-03-07 12:28:19 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    resizeDiv();
+});
+
+$('html').onresize = function() {
+    resizeDiv();
+};
+
+function resizeDiv() {
+    vpw = $(window).width();
+    vph = $(window).height();
+    $('.home-cover').attr('style', '{height:' + vph);
+}
