1d860a3bd7cdcea24b98739adbef156faf1b95d4
Ryan Fanning <fanningrm@gmail.com>
2014-03-10 17:43:07 -0400

    Edited header and cover photo

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-    resizeDiv();
-});
+// $(document).ready(function(){
+//     resizeDiv();
+// });
 
-$('html').onresize = function() {
-    resizeDiv();
-};
+// $('html').onresize = function() {
+//     resizeDiv();
+// };
 
-function resizeDiv() {
-    vpw = $(window).width();
-    vph = $(window).height();
-    $('.home-cover').attr('style', '{height:' + vph);
-}
+// function resizeDiv() {
+//     vpw = $(window).width();
+//     vph = $(window).height();
+//     $('.home-cover').attr('style', '{height:' + vph);
+// }
