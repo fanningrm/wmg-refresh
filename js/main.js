be75504b621b54e87023fdfb1808e7d7f6dc0df4
Ryan Fanning <fanningrm@gmail.com>
2014-03-26 18:32:39 -0400

    streamline js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,12 @@
 // Mobile Menu Functionality
 
 var mainMenu = $('#main-menu ul li');
-var w = $(window).width();
 
 $('.menu-icon').on('click', function(e) {
     e.preventDefault();
-
-    if(mainMenu.hasClass('show')){
-        mainMenu.removeClass('show').addClass('hide');
-    }
-    else {
-        mainMenu.removeClass('hide').addClass('show');
-    }
+    mainMenu.toggleClass('show');
 });
 
-$(window).resize(function(){
-    if(w > 767) {
-        mainMenu.removeClass('show hide');
-    }
-});
 
 // End Mobile Menu Functionality
 
