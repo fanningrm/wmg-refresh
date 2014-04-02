1fbe7866d26cdc9ec274f37191a1da4710e2a01f
Ryan Fanning <fanningrm@gmail.com>
2014-04-02 10:45:59 -0400

    smooth scroll, homepage edits

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ $('.menu-icon').on('click', function(e) {
 
 // End Mobile Menu Functionality
 
+
+//Smooth ScrollTo
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+//End Smooth ScrollTo
